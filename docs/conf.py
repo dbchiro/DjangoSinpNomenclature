3582c37e5601b8e8461d0e2e44c78aecf3a7e52c
hypsug0 <fcloitre@dbchiro.org>
2021-07-19 00:24:05 +0200

    update poetry config

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, dbChiro project <project@dbchiro.org>'
 author = 'dbChiro project <project@dbchiro.org>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------

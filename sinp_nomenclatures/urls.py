36b6a6836424a564b81fc9ca1aec4f4d50a9fd7f
Fred C <42280769+hypsug0@users.noreply.github.com>
2022-11-10 23:51:28 +0100

    fix: better routes

--- sinp_nomenclatures/urls.py
+++ sinp_nomenclatures/urls.py
@@ -1,23 +1,14 @@
-from django.urls import path
-
-from .views import (
-    NomenclatureViewset,
-    SourceViewset,
-    TypeViewset,
-)
-from django.urls import path, include
-
+from django.urls import include, path
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+from .views import NomenclatureViewset, SourceViewset, TypeViewset
+
+router = routers.SimpleRouter(trailing_slash=False)
 app_name = "sinp_nomenclatures"
 
 
+router.register(r"nomenclatures/sources", SourceViewset)
+router.register(r"nomenclatures/types", TypeViewset)
+router.register(r"nomenclatures/nomenclatures", NomenclatureViewset)
 
-router.register(r'sources', SourceViewset)
-router.register(r'types', TypeViewset)
-router.register(r'nomenclatures', NomenclatureViewset)
-
-urlpatterns = [
-    path('', include(router.urls))
-]
+urlpatterns = [path("", include(router.urls))]
